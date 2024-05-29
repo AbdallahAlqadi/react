b42b019f414f3635e9e1edf03cf967628baba832
AbdallahAlqadi <161605945+AbdallahAlqadi@users.noreply.github.com>
2024-05-29 15:02:48 +0300

    add

--- my-react-app/src/Componant/NewHeades.js
+++ my-react-app/src/Componant/NewHeades.js
@@ -1,11 +1,12 @@
 
-
+import * as React from 'react';
+import Button from '@mui/material/Button';
 function NewHeades() {
     return (
         
   <div>
     <h1>this is a new header</h1>
-    
+    <Button variant="contained">Hello world</Button>
     
     </div>
     );
