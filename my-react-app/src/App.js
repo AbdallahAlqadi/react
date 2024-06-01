95f6e5e47df2ce174b7cb331c5ec66fa84cfa56f
AbdallahAlqadi <161605945+AbdallahAlqadi@users.noreply.github.com>
2024-06-01 11:38:43 +0300

    add

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -18,10 +18,21 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import logo from './logo.svg';
 import ReactDOM from 'react-dom'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import { Container,Grid } from '@mui/material';
 
 
 function App() { //جزئيه routes بتتحقق اذا كبست على NewHeaders بعرض  محتواها
 
+  const socialMediaLinks = {
+    facebook: '#',
+    twitter: '#',
+    instagram: '#',
+  };
+  var maxWidth;
+  
   return (
 <BrowserRouter>
 
@@ -57,8 +68,72 @@ function App() { //جزئيه routes بتتحقق اذا كبست على NewHead
       <Route path='/counter' element={<Counter />}/>
   </Routes>
 
+<div className='footer'>
+ <Box
+      sx={{
+        bgcolor: 'background.paper',
+        color: 'text.secondary',
+        py: 3,
+        borderTop: '1px solid',
+        borderColor: 'divider',
+      }}
+    >
+      <Container maxWidth={false}>
+        <Grid container spacing={2} justifyContent="space-between">
+          <Grid item xs={12} sm={6} md={3}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              Brand Name
+            </Typography>
+            {/* Add your logo component or image here */}
+          </Grid>
+          <Grid item xs={6} sm={3} md={2}>
+            <Typography variant="subtitle1" color="text.primary" gutterBottom>
+              PRODUCT
+            </Typography>
+            <Link href="#" color="inherit" display="block">Features</Link>
+            <Link href="#" color="inherit" display="block">Integrations</Link>
+            <Link href="#" color="inherit" display="block">Pricing</Link>
+            <Link href="#" color="inherit" display="block">FAQ</Link>
+          </Grid>
+          <Grid item xs={6} sm={3} md={2}>
+            <Typography variant="subtitle1" color="text.primary" gutterBottom>
+              COMPANY
+            </Typography>
+            <Link href="#" color="inherit" display="block">About Us</Link>
+            <Link href="#" color="inherit" display="block">Careers</Link>
+            <Link href="#" color="inherit" display="block">Privacy Policy</Link>
+            <Link href="#" color="inherit" display="block">Terms of Service</Link>
+          </Grid>
+          <Grid item xs={6} sm={3} md={2}>
+            <Typography variant="subtitle1" color="text.primary" gutterBottom>
+              DEVELOPERS
+            </Typography>
+            <Link href="#" color="inherit" display="block">Public API</Link>
+            <Link href="#" color="inherit" display="block">Documentation</Link>
+            <Link href="#" color="inherit" display="block">Guides</Link>
+          </Grid>
+          <Grid item xs={6} sm={3} md={2}>
+            <Typography variant="subtitle1" color="text.primary" gutterBottom>
+              SOCIAL MEDIA
+            </Typography>
+            <IconButton aria-label="Facebook" color="inherit" component="a" href={socialMediaLinks.facebook}>
+              <FacebookIcon />
+            </IconButton>
+            <IconButton aria-label="Twitter" color="inherit" component="a" href={socialMediaLinks.twitter}>
+              <TwitterIcon />
+            </IconButton>
+            <IconButton aria-label="Instagram" color="inherit" component="a" href={socialMediaLinks.instagram}>
+              <InstagramIcon />
+            </IconButton>
+          </Grid>
+        </Grid>
+        <Typography variant="body2" color="text.secondary" align="center" sx={{ pt: 4 }}>
+          © 2024 Company Co. All rights reserved.
+        </Typography>
+      </Container>
+    </Box>
 
-
+    </div>
 </BrowserRouter>
   );
 }
