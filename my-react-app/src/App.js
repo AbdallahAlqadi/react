ab395a348118146ec2520f20870196b75fe55b3c
AbdallahAlqadi <161605945+AbdallahAlqadi@users.noreply.github.com>
2024-06-01 08:28:36 +0300

    addd

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -9,16 +9,9 @@ import reportWebVitals from './reportWebVitals';
 import ClasssPage from './Componant/Classcomponant';
 import React from 'react';
 import logo from './logo.svg';
-
-
 import ReactDOM from 'react-dom'
 
-const NavPage=()=>NewHeades  // بستدعي NewHeades
-const page1=()=>Page1
-const page2=()=>Page2
-const page3=()=>Page3
-const counter=()=>Counter
-const ReportWebVitals=()=>reportWebVitals
+
 function App() { //جزئيه routes بتتحقق اذا كبست على NewHeaders بعرض  محتواها
 
   return (
