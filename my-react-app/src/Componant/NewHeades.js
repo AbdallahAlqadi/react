f796ed5a4785ff295d3f8ccc0835c11b1be8e4b6
AbdallahAlqadi <161605945+AbdallahAlqadi@users.noreply.github.com>
2024-06-07 10:47:27 +0300

    add

--- my-react-app/src/Componant/NewHeades.js
+++ my-react-app/src/Componant/NewHeades.js
@@ -1,12 +1,15 @@
 
 import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
 function NewHeades() {
+  const navigate=useNavigate();
     return (
         
   <div>
     <h1>this is a new header</h1>
-    <Button variant="contained">Hello world</Button>
+    <Button variant="contained" onClick={()=>{navigate(`/index`)}}>Hello world</Button>
+   
     
     </div>
     );
--- my-react-app/src/Componant/Page3.js
+++ my-react-app/src/Componant/Page3.js
@@ -1,9 +1,12 @@
+import React, { useState } from "react";
 function Page3() {
+  var [value,Setvalue]=useState(0);
     return (
-        
+      
   <div>
-    <h1>this is a Page3</h1>
-  
+
+    <h1>this is a Page3 {value}</h1>
+  <button onClick={()=>Setvalue(value++)}>click</button>
     </div>
     );
   }
